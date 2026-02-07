4d524998a60f7dae228e440b093226e37bc7b254
jd <hashimweyesujeandedie@gmail.com>
2026-02-07 23:32:06 +0200

    Added slideshow codes to 'index.js'

--- index.js
+++ index.js
@@ -3,5 +3,26 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n){
-    showSlides(slideIndex =)
+    showSlides(slideIndex += n);
+}
+
+//Thumbnail image controls
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementByClassName("dot");
+    if (n > slides.length){slideIndex =1 }
+    if(n < 1 ){ slideIndex = slide.length}
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    dots[slideIndex - 1].className += "active";
 }
